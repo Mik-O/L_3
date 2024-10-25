a8a3eaffa19d2a82c931fcd3aa153e7ce90fa828
mikle <mikle.kontinent@gmail.com>
2024-10-25 23:58:47 +0700

    initial commit

---
+++ L_3/L_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L_3
+//
+//  Created by Таня Кожевникова on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
